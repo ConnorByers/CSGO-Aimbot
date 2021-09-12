951dd62a5d63d56c9be14e5b90bfccd31b1945aa
Connor Byers <connorbyers27@gmail.com>
2021-09-11 21:49:55 -0400

    Added model and basic filtering of matches

--- main.py
+++ main.py
@@ -2,15 +2,50 @@ import numpy as np
 import cv2
 from mss import mss
 from PIL import Image
+from time import time
+from pynput import keyboard
+import math
 
-bounding_box = {'top': 25, 'left': 0, 'width': 1280, 'height': 800}
+# 275 x 850
+bounding_box = {'top': 150, 'left': 150, 'width': 1000, 'height': 425}
 
 sct = mss()
+sct_img = np.array(sct.grab(bounding_box))
+cur_time = time()
+
+model = cv2.CascadeClassifier('cascade/cascade.xml')
+
+def isValidCounterTerroristColor(color, corner):
+    r, g, b = int(color[2]), int(color[1]), int(color[0])
+    c_r, c_g, c_b = int(corner[2]), int(corner[1]), int(corner[0])
+    hsp = math.sqrt( 0.299*r*r + 0.587*g*g + 0.114*b*b )
+    corner_hsp = math.sqrt( 0.299*c_r*c_r + 0.587*c_g*c_g + 0.114*c_b*c_b )
+    if hsp < 30 and (corner_hsp+1)/(hsp+1) > 2 and ((abs(2*r - b - g) < 15) or (abs(b - 2 * r) < 10 and abs(b - 2 * g) < 10)):
+        return True
+    #if r + b + g - (r + b + g)/3 < 10:
+    #    return True
+    return False
+
+def isCounterTerrorist(img, x, y, w, h):
+    mid_pos_x, mid_pos_y = x + w // 2 , y + h // 2
+    if y > 30 and y < 350 and isValidCounterTerroristColor(img[mid_pos_y, mid_pos_x], img[mid_pos_y, x]):
+        return True
+    return False
 
 while True:
-    sct_img = sct.grab(bounding_box)
-    cv2.imshow('screen', np.array(sct_img))
+    sct_img = np.array(sct.grab(bounding_box))
 
-    if (cv2.waitKey(1) & 0xFF) == ord('q'):
+    rec = model.detectMultiScale(sct_img)
+    #rec, weights = cv2.groupRectangles(np.array(rec).tolist(), 0, 1000)
+    for (x, y, w, h) in rec:
+        top_left_point = (x, y)
+        bottom_right_point = (x + w, y + h)
+        if isCounterTerrorist(sct_img, x, y, w, h):
+            cv2.rectangle(sct_img, top_left_point, bottom_right_point, (0, 255, 0))
+    
+    cv2.imshow('screen', sct_img)
+    cur_time = time()
+    key = cv2.waitKey(1)
+    if key == ord('q'):
         cv2.destroyAllWindows()
         break
---
+++ trainingdatageneration.py
@@ -0,0 +1,43 @@
+import numpy as np
+import cv2
+from mss import mss
+from PIL import Image
+from time import time
+from pynput import keyboard
+
+# 275 x 850
+bounding_box = {'top': 150, 'left': 150, 'width': 1000, 'height': 425}
+
+sct = mss()
+sct_img = np.array(sct.grab(bounding_box))
+cur_time = time()
+
+def on_press(key):
+    if key == keyboard.KeyCode.from_char('f'):
+        cv2.imwrite('positive/{}.jpg'.format(cur_time), sct_img)
+    elif key == keyboard.KeyCode.from_char('g'):
+        cv2.imwrite('negative/{}.jpg'.format(cur_time), sct_img)
+
+def on_release(key):
+    pass
+    #if key == keyboard.KeyCode.from_char('q'):
+    #    return False
+
+listener = keyboard.Listener(
+    on_press=on_press,
+    on_release=on_release)
+listener.start()
+
+while True:
+    sct_img = np.array(sct.grab(bounding_box))
+    sct_img = cv2.cvtColor(sct_img, cv2.COLOR_RGB2GRAY)
+    cv2.imshow('screen', sct_img)
+    cur_time = time()
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        cv2.destroyAllWindows()
+        break
+    #elif key == ord('f'):
+    #    cv2.imwrite('positive/{}.jpg'.format(cur_time), sct_img)
+    #elif key == ord('d'):
+    #    cv2.imwrite('negative/{}.jpg'.format(cur_time), sct_img)
